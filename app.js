978239e2fe0b82ab8896a046b23d0ea112652128
vvictorious <vvictorious30@gmail.com>
2018-03-28 20:53:11 -0600

    experimenting

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ let margValue = 0;
 // let redLeft=0;
 
 
+
+
+
+
+
 window.addEventListener('keydown', (event) =>{
 	(event.keycode == 39) 
 		console.log('right arrow key pressed');
@@ -15,6 +20,41 @@ window.addEventListener('keydown', (event) =>{
 		blue.style.marginLeft = (margValue += 50) + 'px'
 	 
 })
+
+
+
+
+
+
+// document.addEventListener('keydown', (event) =>{
+// 	if (event.keycode == 39) {
+
+
+// 		console.log('right arrow key pressed');
+// 		// margValue+=50 + "px"
+// 		blue.style.marginLeft = (margValue += 40) + 'px'
+// 		if(margValue >= 460) {
+// 			alert('Player 1 wins!')
+// 		}	//end of if statement on line 12
+// 	} 
+// })
+
+// document.addEventListener('keydown', (event) =>{
+// 	if (event.keycode == 68) {} 
+		
+// 		console.log('d key is pressed');
+// 		// margValue+=50 + "px"
+// 		red.style.marginLeft = (margValue += 40) + 'px'
+// 		if(margValue >= 460) {
+// 			alert('Player 2 wins!')
+// 		}
+	 
+// })
+
+
+
+
+
 
 
 
